e61a7d9c98c5e7cf8eb2e326cb648d90dab32ebd
aluno <aluno@lab01-24>
2026-03-02 09:05:16 -0300

    minha primeia atualização

---
+++ programacao01/src/main/java/com/mycompany/programacao01/Programacao01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacao01;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Programacao01 {
+
+   private String nome;
+    private Date dataNascimento;
+    private String cpf;
+    private char genero;
+    private String matricula;
+    private String email;
+    
+}
