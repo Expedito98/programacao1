bca4bfe09d0edb59ff98a173d032b21287a73e16
aluno <aluno@lab01-25>
2026-03-10 14:13:28 -0300

    novas vairavel teste1

---
+++ esrudante/src/main/java/com/mycompany/esrudante/teste1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.esrudante;
+
+/**
+ *
+ * @author aluno
+ */
+public class teste1 {
+    
+    public static void main(String[] args) {       
+     Estudante cleiton = new Estudante();
+     Estudante gustavo = new Estudante();
+     Estudante bababado = new Estudante();
+     
+     cleiton.netNome()
+
+       }
+
+    
+}
