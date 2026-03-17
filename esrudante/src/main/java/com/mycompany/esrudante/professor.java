432da8ad633e6109970bc5e375ea3f51fd1731b6
aluno <aluno@lab01-25>
2026-03-17 14:14:06 -0300

    novas coisas

---
+++ esrudante/src/main/java/com/mycompany/esrudante/professor.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.esrudante;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class professor {
+
+    private String nomeCompleto;
+
+    private int indentiInstitucional;
+    private String email;
+    private String tituloAcademica;
+    private String AreaDeAtuacao;
+    private String RegimeTrabalho;
+
+}
+/*private Date dataNascimento;
+    private String cpf;
+    private char genero;
+    private String matricula;
+    private String email;
+    private String niveldeEnsino;
+    private int telefone;
+    private int ingresso;
+    private int semestre;
+    private String academica;
+    private String digito;*/
