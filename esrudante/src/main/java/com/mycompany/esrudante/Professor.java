64a87ddc3e158b0f767601c679c47d5ecaf64d1e
aluno <aluno@lab01-25>
2026-03-23 09:33:18 -0300

    add

---
+++ esrudante/src/main/java/com/mycompany/esrudante/Professor.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.esrudante;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Professor {
+
+    private String nomeCompleto;
+    private int IndentifiInstitucional;
+    private String email;
+     private int telefone;
+    private String tituloAcademica;
+    private String areaDeAtuacao;
+    private String regimeTrabalho;
+    private Date dataIngreso;
+    private String formacaoGraduacao;
+    private String metradoOuDoutorado;
+    
+
+      public String getnomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String _nomeCompleto) {
+        nomeCompleto = _nomeCompleto;
+    }
+    
+    
+    public int setIndentifiInstitucional() {
+
+        return IndentifiInstitucional;
+
+    }
+
+    public void setIndentifiInstitucional(int _IndentifiInstitucional) {
+        IndentifiInstitucional = _IndentifiInstitucional;
+    }
+    
+    
+    public String getEmail(){
+        return email;
+    
+    }
+      public void setEmail(String _email) {
+        email = _email;
+    }
+    
+     
+    public int getTelefone(){
+        return telefone;
+        
+    }
+    public void setTelefone(int _telefone){
+        telefone = _telefone;
+    }
+    
+    public String getTituloAcademica(){
+        
+        return tituloAcademica;
+        
+    }
+    public void setTituloAcademica (String _tituloAcademica){
+       tituloAcademica = _tituloAcademica;
+    
+    }
+    
+     public String getAreaDeAtuacao(){
+        
+        return areaDeAtuacao;
+        
+    }
+    public void setAreaDeAtuacao (String _areaDeAtuacao){
+      areaDeAtuacao = _areaDeAtuacao;
+    
+    }
+      public String getRegimeTrabalho(){
+        
+        return regimeTrabalho;
+        
+    }
+    public void setRegimeTrabalaho (String _regimeTrabalho){
+     regimeTrabalho= _regimeTrabalho;
+    
+    }
+    public Date getDataIngresso(){
+        return dataIngreso;
+    }
+            
+    public void setDataIngreso(Date _dataIngreso){
+        dataIngreso= _dataIngreso;
+    }
+    
+      public String getFormacaoGraduacao(){
+        
+        return formacaoGraduacao;
+        
+    }
+    public void setFormacaoGraduacao (String _formacaoGraduacao){
+     formacaoGraduacao= _formacaoGraduacao;
+    }
+    
+    
+     public String getMetradoOuDoutorado(){
+        
+        return metradoOuDoutorado;
+        
+    }
+    public void setMetradoOuDoutorado (String _metradoOuDoutorado){
+    metradoOuDoutorado= _metradoOuDoutorado;
+    
+    }
+    
+    
+    
+}
+
