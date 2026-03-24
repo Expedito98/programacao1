6d9bf129f871fc25bdd1e3230fa628da28d071f2
aluno <aluno@lab01-25>
2026-03-24 14:13:15 -0300

    novo

---
+++ esrudante/src/main/java/com/mycompany/esrudante/Turma.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.esrudante;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author aluno
+ */
+public class Turma {
+    
+    private Estudante discente = new Estudante(); 
+    private Disciplina disciplina = new Disciplina();
+    private Professor professor = new Professor();
+    
+    private Set<Estudante>Estudantes = new HashSet<Estudante>();
+    private Set<Disciplina>Disciplina = new HashSet<Disciplina>();
+    private Set<Professor>Professor = new HashSet<Professor>();
+    
+    
+}
