4780331e8c2c7aa9a2717d4d1f486078540c3627
aluno <aluno@lab01-25>
2026-03-24 13:45:15 -0300

    novos

--- esrudante/src/main/java/com/mycompany/esrudante/Professor.java
+++ esrudante/src/main/java/com/mycompany/esrudante/Professor.java
@@ -13,111 +13,22 @@ import java.util.Date;
 public class Professor {
 
     private String nomeCompleto;
-    private int IndentifiInstitucional;
+    private int indentiInstitucional;
     private String email;
-     private int telefone;
     private String tituloAcademica;
-    private String areaDeAtuacao;
-    private String regimeTrabalho;
-    private Date dataIngreso;
-    private String formacaoGraduacao;
-    private String metradoOuDoutorado;
-    
+    private String AreaDeAtuacao;
+    private String RegimeTrabalho;
 
-      public String getnomeCompleto() {
-        return nomeCompleto;
-    }
-
-    public void setNomeCompleto(String _nomeCompleto) {
-        nomeCompleto = _nomeCompleto;
-    }
-    
-    
-    public int setIndentifiInstitucional() {
-
-        return IndentifiInstitucional;
-
-    }
-
-    public void setIndentifiInstitucional(int _IndentifiInstitucional) {
-        IndentifiInstitucional = _IndentifiInstitucional;
-    }
-    
-    
-    public String getEmail(){
-        return email;
-    
-    }
-      public void setEmail(String _email) {
-        email = _email;
-    }
-    
-     
-    public int getTelefone(){
-        return telefone;
-        
-    }
-    public void setTelefone(int _telefone){
-        telefone = _telefone;
-    }
-    
-    public String getTituloAcademica(){
-        
-        return tituloAcademica;
-        
-    }
-    public void setTituloAcademica (String _tituloAcademica){
-       tituloAcademica = _tituloAcademica;
-    
-    }
-    
-     public String getAreaDeAtuacao(){
-        
-        return areaDeAtuacao;
-        
-    }
-    public void setAreaDeAtuacao (String _areaDeAtuacao){
-      areaDeAtuacao = _areaDeAtuacao;
-    
-    }
-      public String getRegimeTrabalho(){
-        
-        return regimeTrabalho;
-        
-    }
-    public void setRegimeTrabalaho (String _regimeTrabalho){
-     regimeTrabalho= _regimeTrabalho;
-    
-    }
-    public Date getDataIngresso(){
-        return dataIngreso;
-    }
-            
-    public void setDataIngreso(Date _dataIngreso){
-        dataIngreso= _dataIngreso;
-    }
-    
-      public String getFormacaoGraduacao(){
-        
-        return formacaoGraduacao;
-        
-    }
-    public void setFormacaoGraduacao (String _formacaoGraduacao){
-     formacaoGraduacao= _formacaoGraduacao;
-    }
-    
-    
-     public String getMetradoOuDoutorado(){
-        
-        return metradoOuDoutorado;
-        
-    }
-    public void setMetradoOuDoutorado (String _metradoOuDoutorado){
-    metradoOuDoutorado= _metradoOuDoutorado;
-    
-    }
-    
-    
-    
 }
-
+/*private Date dataNascimento;
+    private String cpf;
+    private char genero;
+    private String matricula;
+    private String email;
+    private String niveldeEnsino;
+    private int telefone;
+    private int ingresso;
+    private int semestre;
+    private String academica;
+    private String digito;*/
+    
