4b375941b4da7b1ed0bcd410afe33c3b1a62c5ff
aluno <aluno@lab01-25>
2026-04-06 09:32:26 -0300

    aa

---
+++ esrudante/src/main/java/atividade0604/Listaecadeado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atividade0604;
+
+/**
+ *
+ * @author aluno
+ */
+public class Listaecadeado {
+    private No primeiroelemento;
+}
