f476670d12ceada762eaa2665bd39889ecf4bbd7
aluno <aluno@lab01-25>
2026-04-07 17:14:04 -0300

    mudança de nomes

---
+++ esrudante/src/main/java/com/mycompany/esrudante/Materia.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.esrudante;
+
+/**
+ *
+ * @author aluno
+ */
+public class Materia {
+
+    private int codigo;
+    private String disciplina;
+    private int cargahoraria;
+    private String emenda;
+    private String objetivosaprendi;
+    private int conteudoprogama;
+    private String refebibliografia;
+    private String organizacao;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int _codigo) {
+        codigo = _codigo;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String _disciplina) {
+        disciplina = _disciplina;
+    }
+
+    public int getCargahoraria() {
+        return cargahoraria;
+    }
+
+    public void setCargahoraria(int _cargahoraria) {
+        cargahoraria = _cargahoraria;
+    }
+
+    public String getEmenda() {
+        return emenda;
+    }
+
+    public void setEmenda(String _emenda) {
+        emenda = _emenda;
+    }
+
+    public String getObjetivosaprendi() {
+        return objetivosaprendi;
+    }
+
+    public void setObjetivosaprendi(String _objetivosaprendi) {
+        objetivosaprendi = _objetivosaprendi;
+    }
+
+    public int getConteudoprogama() {
+        return conteudoprogama;
+    }
+
+    public void setConteudoprogama(int _conteudoprogama) {
+        conteudoprogama = _conteudoprogama;
+    }
+
+    public String getRefebibliografia() {
+        return refebibliografia;
+    }
+
+    public void setRefebibliografia(String _refebibliografia) {
+        refebibliografia = _refebibliografia;
+    }
+
+    public String getOrganizacao() {
+        return organizacao;
+    }
+
+    public void setOrganizacao(String _organizacao) {
+        organizacao = _organizacao;
+    }
+
+}
